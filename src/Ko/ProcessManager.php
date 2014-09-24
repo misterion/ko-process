c2eaf4a70f929097781cc470d95d6311df10a48e
Nikolay Bondarenko <misterionkell@gmail.com>
2014-09-24 23:06:47 +0400

    The SM should be allocated in master

--- src/Ko/ProcessManager.php
+++ src/Ko/ProcessManager.php
@@ -151,6 +151,7 @@ class ProcessManager implements \Countable
     protected function createProcess(callable $callable)
     {
         $p = new Process($callable);
+        $p->setSharedMemory(new SharedMemory());
         $p->on('exit', function ($pid) use ($p) {
             $this->childProcessDie($pid, $p->getStatus());
         });
