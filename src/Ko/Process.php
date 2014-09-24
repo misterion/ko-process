a8718b0cf85ca2a19f2bb0f7021f41e48b24568a
Craig Duncan <git@duncanc.co.uk>
2014-09-24 19:16:59 +0100

    Have the Process class implement ArrayAccess
    This allows easier use of SharedMemory from processes

--- src/Ko/Process.php
+++ src/Ko/Process.php
@@ -8,7 +8,7 @@ namespace Ko;
  * @author Nikolay Bondarenko <misterionkell@gmail.com>
  * @version 1.0
  */
-class Process
+class Process implements \ArrayAccess, \Countable
 {
     use Mixin\ProcessTitle;
     use Mixin\EventEmitter {
@@ -243,5 +243,89 @@ class Process
     {
         pcntl_signal_dispatch();
         return $this;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     *
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists($offset)
+    {
+        return $this->sharedMemory->offsetExists($offset);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     *
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        return $this->sharedMemory->offsetGet($offset);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->sharedMemory->offsetSet($offset, $value);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        $this->sharedMemory->offsetUnset($offset);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     */
+    public function count()
+    {
+        return $this->sharedMemory->count();
     }
 }
